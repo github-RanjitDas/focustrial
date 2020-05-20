a1e96de3b9627a2fa0fde42c0180019f94c7c26f
Andres Florez <afloreza@psl.com.co>
2020-05-20 10:52:09 -0500

    feature(*): end BWC connection after 5 minutes
    - Connection ends after 5 minutes
    - Apps restarts to login activity
    - Added some missing strings
    
    #FMA-239

--- presentation/src/main/java/com/lawmobile/presentation/ui/base/BaseActivity.kt
+++ presentation/src/main/java/com/lawmobile/presentation/ui/base/BaseActivity.kt
@@ -1,6 +1,12 @@
 package com.lawmobile.presentation.ui.base
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.Process
+import android.view.WindowManager
+import com.lawmobile.presentation.ui.login.LoginActivity
 import dagger.android.support.DaggerAppCompatActivity
+import java.sql.Timestamp
 import javax.inject.Inject
 import kotlin.system.exitProcess
 
@@ -8,6 +14,8 @@ open class BaseActivity : DaggerAppCompatActivity() {
 
     @Inject
     lateinit var baseViewModel: BaseViewModel
+    private lateinit var lastInteraction: Timestamp
+    var isRecordingVideo: Boolean = false
 
     fun killApp() {
         baseViewModel.deactivateCameraHotspot()
@@ -15,7 +23,37 @@ open class BaseActivity : DaggerAppCompatActivity() {
         exitProcess(0)
     }
 
+    fun restartApp() {
+        val intent: Intent? = this.baseContext.packageManager
+            .getLaunchIntentForPackage(this.baseContext.packageName)
+        intent?.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        Process.killProcess(Process.myPid())
+        exitProcess(0)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        super.onCreate(savedInstanceState)
+        lastInteraction = Timestamp(System.currentTimeMillis())
+    }
+
+    override fun onUserInteraction() {
+        super.onUserInteraction()
+        if (!isRecordingVideo && checkIfSessionIsExpired() && this !is LoginActivity) {
+            return
+        }
+        lastInteraction = Timestamp(System.currentTimeMillis())
+    }
+
+    fun checkIfSessionIsExpired(): Boolean {
+        val timeNow = Timestamp(System.currentTimeMillis())
+        return (timeNow.time - lastInteraction.time) > MAX_TIME_SESSION
+    }
+
     companion object {
         const val PERMISSION_FOR_LOCATION = 100
+        const val MAX_TIME_SESSION = 300000
     }
+
 }
