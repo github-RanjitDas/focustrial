ecf2cbad332361712f0069524ab8664fdb32c381
Kevin Meneses <kevin.menesesp@perficient.com>
2020-10-01 14:46:39 -0500

    fix(live stream): main screen UI is not responsive
    - add guidelines and constraints to layout
    - update main screen buttons dimens
    
    #FMA-1320

--- presentation/src/main/java/com/lawmobile/presentation/widgets/CustomRecordButton.kt
+++ presentation/src/main/java/com/lawmobile/presentation/widgets/CustomRecordButton.kt
@@ -3,6 +3,7 @@ package com.lawmobile.presentation.widgets
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import com.lawmobile.presentation.R
 import com.safefleet.mobile.commons.widgets.SafeFleetClickable
@@ -15,13 +16,16 @@ class CustomRecordButton @JvmOverloads constructor(
 ) : SafeFleetClickable(context, attrs, defStyleAttr), View.OnClickListener {
 
     private var buttonText: TextView
+    private var buttonIcon: ImageView
 
     init {
         View.inflate(context, R.layout.button_custom_record, this)
         buttonText = findViewById(R.id.textViewButtonRecord)
+        buttonIcon = findViewById(R.id.imageViewCustomRecord)
         setOnClickListener(this)
         buttonCustomRecord.setOnClickListener(this)
         textViewButtonRecord.setOnClickListener(this)
+        imageViewCustomRecord.setOnClickListener(this)
     }
 
     override fun isActivated(): Boolean {
@@ -45,7 +49,7 @@ class CustomRecordButton @JvmOverloads constructor(
         with(buttonCustomRecord) {
             if (isActivated) {
                 setBackgroundResource(R.drawable.background_recording_active)
-                setImageResource(R.drawable.ic_record_active)
+                buttonIcon.setImageResource(R.drawable.ic_record_active)
                 buttonText.text = context.getString(R.string.stop)
                 buttonText.setTextColor(context.getColor(R.color.red))
 
@@ -57,7 +61,7 @@ class CustomRecordButton @JvmOverloads constructor(
                 )
             } else {
                 setBackgroundResource(R.drawable.background_live_view_buttons)
-                setImageResource(R.drawable.ic_video)
+                buttonIcon.setImageResource(R.drawable.ic_video)
                 buttonText.text = context.getString(R.string.record_video)
                 buttonText.setTextColor(context.getColor(R.color.white))
 
