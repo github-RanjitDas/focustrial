6de07f29018e05c579a7b03f21af05344cdf4208
Kevin Meneses <kevin.menesesp@perficient.com>
2021-11-25 15:18:48 -0500

    chore(*): change build version
    - change app version
    
    #FMA-2795

--- buildSrc/src/main/java/dependencies/AppConfig.kt
+++ buildSrc/src/main/java/dependencies/AppConfig.kt
@@ -10,7 +10,7 @@ object AppConfig {
     const val buildVersion = 95
     const val major = 3
     const val minor = 6
-    const val patch = 5
+    const val patch = 6
     const val versionName = "$major.$minor.$patch"
 
     val jvmTarget = JavaVersion.VERSION_1_8
