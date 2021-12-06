07a12d0634eb079242029ce3ddea07cb3e25edec
Kevin Meneses <kevin.menesesp@perficient.com>
2021-12-06 10:51:01 -0500

    build(*): change the app version
    - change the app version as requested
    
    #FMA-2873

--- buildSrc/src/main/java/dependencies/AppConfig.kt
+++ buildSrc/src/main/java/dependencies/AppConfig.kt
@@ -7,10 +7,10 @@ object AppConfig {
     const val compileSdkVersion = 30
     const val minSdkVersion = 23
     const val targetSdkVersion = 30
-    const val buildVersion = 95
+    const val buildVersion = 96
     const val major = 3
-    const val minor = 6
-    const val patch = 6
+    const val minor = 7
+    const val patch = 0
     const val versionName = "$major.$minor.$patch"
 
     val jvmTarget = JavaVersion.VERSION_1_8
