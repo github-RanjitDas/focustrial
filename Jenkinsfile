84d1f8ba2290a194ca14f28cf6deb34915c7b283
gajraj <gajraj.chouhan@global-csg.com>
2023-04-28 10:25:18 +0530

    fix(CCPassword):FMA-3554: Android - FMA to validate Command Center device password

--- Jenkinsfile
+++ Jenkinsfile
@@ -54,7 +54,7 @@ node('jenkins-builds-slave') {
             stage('Unit Tests') {
                 logger.stage()
                 timeout(7) {
-                    sh "./gradlew testDebugUnitTest --stacktrace"
+                    sh "./gradlew testDebugUnitTestCoverage --stacktrace"
                 }
 			}
             if(env.BRANCH_NAME != 'develop' && env.BRANCH_NAME != 'master'){
--- presentation/src/test/java/com/lawmobile/presentation/ui/base/appBar/x2/AppBarX2ViewModelTest.kt
+++ presentation/src/test/java/com/lawmobile/presentation/ui/base/appBar/x2/AppBarX2ViewModelTest.kt
@@ -1,61 +1,67 @@
-// package com.lawmobile.presentation.ui.base.appBar.x2
-//
-// import com.lawmobile.domain.usecase.events.EventsUseCase
-// import com.lawmobile.presentation.InstantExecutorExtension
-// import com.safefleet.mobile.kotlin_commons.helpers.Result
-// import io.mockk.coEvery
-// import io.mockk.coVerify
-// import io.mockk.mockk
-// import kotlinx.coroutines.ExperimentalCoroutinesApi
-// import kotlinx.coroutines.test.TestCoroutineDispatcher
-// import kotlinx.coroutines.test.runBlockingTest
-// import org.junit.Assert
-// import org.junit.Ignore
-// import org.junit.jupiter.api.Test
-// import org.junit.jupiter.api.TestInstance
-// import org.junit.jupiter.api.extension.ExtendWith
-//
-// @ExperimentalCoroutinesApi
-// @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-// @ExtendWith(InstantExecutorExtension::class)
-// internal class AppBarX2ViewModelTest {
-//
-//    private val eventsUseCase: EventsUseCase = mockk()
-//    private val dispatcher = TestCoroutineDispatcher()
-//
-//    private val appBarX2ViewModel: AppBarX2ViewModel by lazy {
-//        AppBarX2ViewModel(eventsUseCase, dispatcher)
-//    }
-//
-//    @Test
-//    @Ignore("Failing after ExoPlayer implementation, Check later.")
-//    fun getNumberOfPendingNotificationFlow() = runBlockingTest {
-//        coEvery { eventsUseCase.getPendingNotificationsCount() } returns Result.Success(1)
-//        appBarX2ViewModel.getUnreadNotificationCount()
-//        coVerify { eventsUseCase.getPendingNotificationsCount() }
-//    }
-//
-//    @Test
-//    @Ignore("Failing after ExoPlayer implementation, Check later.")
-//    fun getNumberOfPendingNotificationSuccess() = runBlockingTest {
-//        val result = Result.Success(1)
-//        coEvery { eventsUseCase.getPendingNotificationsCount() } returns result
-//        appBarX2ViewModel.getUnreadNotificationCount()
-//        Assert.assertEquals(
-//            result,
-//            appBarX2ViewModel.pendingNotificationCountResult.value
-//        )
-//    }
-//
-//    @Test
-//    @Ignore("Failing after ExoPlayer implementation, Check later.")
-//    fun getNumberOfPendingNotificationError() = runBlockingTest {
-//        val result = Result.Error(Exception())
-//        coEvery { eventsUseCase.getPendingNotificationsCount() } returns result
-//        appBarX2ViewModel.getUnreadNotificationCount()
-//        Assert.assertEquals(
-//            result,
-//            appBarX2ViewModel.pendingNotificationCountResult.value
-//        )
-//    }
-// }
+package com.lawmobile.presentation.ui.base.appBar.x2
+
+import com.lawmobile.domain.usecase.events.EventsUseCase
+import com.lawmobile.presentation.InstantExecutorExtension
+import com.safefleet.mobile.kotlin_commons.helpers.Result
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert
+import org.junit.Before
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExperimentalCoroutinesApi
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(InstantExecutorExtension::class)
+internal class AppBarX2ViewModelTest {
+
+    private val eventsUseCase: EventsUseCase = mockk()
+    private val dispatcher = TestCoroutineDispatcher()
+
+    private val appBarX2ViewModel: AppBarX2ViewModel by lazy {
+        AppBarX2ViewModel(eventsUseCase, dispatcher)
+    }
+
+    @Before
+    fun setup() {
+        val dispatcher = StandardTestDispatcher()
+        Dispatchers.setMain(dispatcher)
+    }
+
+    @Test
+    fun getNumberOfPendingNotificationFlow() = runTest(dispatcher) {
+        coEvery { eventsUseCase.getPendingNotificationsCount() } returns Result.Success(1)
+        appBarX2ViewModel.getUnreadNotificationCount()
+        coVerify { eventsUseCase.getPendingNotificationsCount() }
+    }
+
+    @Test
+    fun getNumberOfPendingNotificationSuccess() = runTest(dispatcher) {
+        val result = Result.Success(1)
+        coEvery { eventsUseCase.getPendingNotificationsCount() } returns result
+        appBarX2ViewModel.getUnreadNotificationCount()
+        Assert.assertEquals(
+            result,
+            appBarX2ViewModel.pendingNotificationCountResult.value
+        )
+    }
+
+    @Test
+    fun getNumberOfPendingNotificationError() = runTest(dispatcher) {
+        val result = Result.Error(Exception())
+        coEvery { eventsUseCase.getPendingNotificationsCount() } returns result
+        appBarX2ViewModel.getUnreadNotificationCount()
+        Assert.assertEquals(
+            result,
+            appBarX2ViewModel.pendingNotificationCountResult.value
+        )
+    }
+}
