299ea91c9f8476595ce4fb77ea74ac1a698a8f99
gajraj <gajraj.chouhan@global-csg.com>
2023-06-07 10:38:25 +0530

    Added Secret Key for for All environments and updated the scope.

--- buildSrc/src/main/java/dependencies/Build.kt
+++ buildSrc/src/main/java/dependencies/Build.kt
@@ -13,12 +13,12 @@ object Build {
     // pending to define client secret for other build variants different to debug
     const val SSO_CLIENT_SECRET_FIELD = "SSO_CLIENT_SECRET"
     const val SSO_CLIENT_SECRET_DEBUG_VALUE = "\"7d2c074d-855a-f5d6-b697-44d2bc602754\""
-    const val SSO_CLIENT_SECRET_RELEASE_VALUE = "\"1\""
-    const val SSO_CLIENT_SECRET_STAGING_VALUE = "\"2\""
-    const val SSO_CLIENT_SECRET_QATEST_VALUE = "\"3\""
+    const val SSO_CLIENT_SECRET_RELEASE_VALUE = "\"d7f1b234-9812-4c56-8ef0-3d9aebc40abc\""
+    const val SSO_CLIENT_SECRET_STAGING_VALUE = "\"c76f923a-541d-48e2-80ab-ec03d7ba95b2\""
+    const val SSO_CLIENT_SECRET_QATEST_VALUE = "\"e1a88c57-6d8f-49be-a37d-2e97ab32156a\""
 
     const val SSO_CLIENT_ID_FIELD = "SSO_CLIENT_ID"
-    const val SSO_CLIENT_ID_VALUE = "\"com.cobantch.focusx1\""
+    const val SSO_CLIENT_ID_VALUE = "\"focusmobileapp\""
 
     const val SSO_CALLBACK_FIELD = "SSO_CALLBACK"
     const val SSO_CALLBACK_VALUE = "\"focus-mobile://callback\""
@@ -29,7 +29,10 @@ object Build {
             "\"openid\"," +
             "\"email\"," +
             "\"profile\"," +
-            "\"hardware-read\"" +
+            "\"hardware-read\"," +
+            "\"tenant-security-api\"," +
+            "\"tenant-security-api-write-users\"," +
+            "\"internal-security-read-users\"" +
             "}"
 
     const val DISCOVERY_URL_FIELD = "DISCOVERY_USER_URL"
