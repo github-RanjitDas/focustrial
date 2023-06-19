248a340aca315656704280230d544343a6d9648a
gajraj <gajraj.chouhan@global-csg.com>
2023-06-19 14:10:41 +0530

    Package Name and version changed for new app

--- buildSrc/src/main/java/dependencies/AppConfig.kt
+++ buildSrc/src/main/java/dependencies/AppConfig.kt
@@ -3,15 +3,15 @@ package dependencies
 import org.gradle.api.JavaVersion
 
 object AppConfig {
-    const val applicationId = "com.cobantch.focusx1"
+    const val applicationId = "com.cobantch.focusx1.v4"
     const val compileSdkVersion = 33
     const val minSdkVersion = 24
     const val targetSdkVersion = 33
     //buildVersion will be different for release
-    const val buildVersion = 12
-    private const val major = 3
-    private const val minor = 8
-    private const val patch = 1
+    const val buildVersion = 1
+    private const val major = 1
+    private const val minor = 0
+    private const val patch = 0
     //versionName will be different for release
     const val versionName = "develop-$major.$minor.$patch"
     val jvmTarget = JavaVersion.VERSION_1_8
